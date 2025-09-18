ec37fbdd64d5210f64c918a9aa0b2d888715cfda
guodevo <wguo446@gmail.com>
2025-09-18 15:51:48 +0200

    works?

---
+++ main.js
@@ -0,0 +1,33 @@
+var list = document.getElementById('list');
+var arrayofspan = list.querySelectorAll('li');
+var arrayofinput = list.querySelectorAll('input');
+
+for (var i=0; i < arrayofspan.length; i++){
+    arrayofspan[i].addEventListener('click', editlist);
+    arrayofinput[i].addEventListener('blur', savechange);
+    arrayofinput[i].addEventListener('keypress', savechangewithkey)
+}
+
+
+function editlist(){
+    console.log(this);
+    this.className = 'edit';
+    var input = this.querySelector('input');
+    input.focus();
+    input.setSelectionRange(null, input.value.length);
+    
+    
+}
+
+function savechange(){
+    console.log(this.value)
+    this.previousElementSibling.innerHTML = this.value
+    this.parentNode.className = ""
+
+}
+function savechangewithkey(event){
+    if (event.which == 13){
+        savechange.call(this);
+    }
+
+}
